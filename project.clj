9f1f077697b3b15ae79aefdaa25a6cd6c8b79076
Halit Olali <halitolali@gmail.com>
2015-03-30 21:01:02 +0200

    bump clojure version

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject zk-web "0.1.0-SNAPSHOT"
   :description "FIXME: write this!"
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
                  [noir "1.3.0-beta3"]
                  [com.netflix.curator/curator-framework "1.1.16"]
                  [com.netflix.curator/curator-test "1.1.16"]]
